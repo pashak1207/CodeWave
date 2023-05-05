5c49f45df2921889b2151ef9155f4eabd06d5546
pashak1207 <pashak12072002@gmail.com>
2023-05-05 19:01:51 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import Header from "./Components/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div className="temp"></div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import '@fontsource/inria-serif'
+import '@fontsource/roboto'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+
+const root:ReactDOM.Root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>
 );
